a5ae07886355864f2d74aed85bd3a99210efcd56
Tremendous1192 <tremendous.duck@gmail.com>
2022-02-05 15:23:52 +0900

    「統計的データ解析の基本(著:山田 秀, 松浦 峻) 発行：サイエンス社」の演習問題や例題を解いていくクラスの作成を始めました。過去に作成したDLL_Toolbox_I_Needからの移植と考えていたが、行列周りの設計を一新したので、ほぼ新規作成です。

---
+++ ComPASS/Test/Books/統計的データ解析の基本/BasicOfStatisticalDataAnalysis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Tremendous1192.SelfEmployed.CoMPASS.Books
+{
+    /// <summary>
+    /// 「統計的データ解析の基本(著:山田 秀, 松浦 峻) 発行：サイエンス社」の演習問題や例題を解いていくクラス。
+    /// 過去に作成したDLL_Toolbox_I_Needからの移植と考えていたが、行列周りの設計を一新したので、ほぼ新規作成です。
+    /// </summary>
+    public partial class BasicOfStatisticalDataAnalysis
+    {
+
+        public static void Quote()
+        {
+            Console.WriteLine("このプログラムは、書籍「統計的データ解析の基本(著:山田 秀, 松浦 峻) 発行：サイエンス社」の演習問題や例題を解いていきます。");
+            Console.WriteLine("https://www.saiensu.co.jp/search/?isbn=978-4-7819-1455-8&y=2019");
+            Console.Write("\n\n");
+        }
+
+    }
+}
